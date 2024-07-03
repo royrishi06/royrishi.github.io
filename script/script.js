8b7fa723338ea9731e1c94191d5588e431f6c4f6
royrishi06 <120101553+royrishi06@users.noreply.github.com>
2024-07-03 14:14:11 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -10,3 +10,25 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
+
+
+// code for the typewriter effect
+document.addEventListener("DOMContentLoaded", function() {
+    function typeWriter(element, text, speed) {
+      let i = 0;
+      function type() {
+        if (i < text.length) {
+          element.innerHTML += text.charAt(i);
+          i++;
+          setTimeout(type, speed);
+        }
+      }
+      type();
+    }
+  
+    const heroTitle = document.getElementById("heroTitle");
+    const heroDesc = document.getElementById("heroDesc");
+  
+    typeWriter(heroTitle, "Hi, I'm Rishi", 100);
+    setTimeout(() => typeWriter(heroDesc, "I am a Software Engineer currently studying at DTU", 100), 1500);
+  });
